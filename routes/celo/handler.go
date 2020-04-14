b7929aeccae7f59fd5b5b06fbf9042e34d063e8d
Khoa Phan <khoaphan89@gmail.com>
2020-04-14 15:47:20 -0400

    Add celo route functions for getting election and validator contracts

---
+++ routes/celo/handler.go
@@ -0,0 +1,41 @@
+package celo
+
+import (
+	"log"
+
+	"github.com/conclave-dev/go-celo/client"
+	"github.com/conclave-dev/go-celo/core/celo"
+	"github.com/conclave-dev/go-celo/core/celo/governance/election"
+	"github.com/conclave-dev/go-celo/core/celo/governance/validators"
+	"github.com/conclave-dev/go-celo/util"
+)
+
+const (
+	celoRPCAPI = "https://geth.celoist.com"
+)
+
+func init() {
+	util.SetupClients()
+}
+
+func getElectionContract() *election.Election {
+	// Use the predefined address for the RegistryContract
+	contractAddress := celo.GetContractAddress(celo.Election, client.EthClient)
+	contract, err := election.NewElection(contractAddress, client.EthClient)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return contract
+}
+
+func getValidatorsContract() *validators.Validators {
+	// Use the predefined address for the RegistryContract
+	contractAddress := celo.GetContractAddress(celo.Validators, client.EthClient)
+	contract, err := validators.NewValidators(contractAddress, client.EthClient)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return contract
+}
