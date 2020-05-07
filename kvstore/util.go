4eb2063428122b7a6f4211b947ae7b29204e1e06
Eko Mirhard <eko.mirhard@gmail.com>
2020-05-07 22:29:49 +0700

    Add hash key prefix builder

--- kvstore/util.go
+++ kvstore/util.go
@@ -5,6 +5,7 @@ import (
 	"log"
 )
 
+// StringifyJSON converts the JSON object into a string
 func StringifyJSON(data interface{}) string {
 	b, err := json.Marshal(data)
 	if err != nil {
@@ -12,4 +13,22 @@ func StringifyJSON(data interface{}) string {
 	}
 
 	return string(b[:])
+}
+
+// GetHashKeyForNetwork returns the hash key for the specified base key and networkID
+func GetHashKeyForNetwork(baseKey string, networkID string) string {
+	var supportedNetwork = false
+
+	for _, identifier := range NETWORK_IDENTIFIERS {
+		if identifier == networkID {
+			supportedNetwork = true
+			break
+		}
+	}
+
+	if !supportedNetwork {
+		panic("Invalid or unsupported network")
+	}
+
+	return baseKey + "-" + networkID
 }
